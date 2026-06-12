283e830a11504eb3ca47a990d4e32353cefcf242
SHIVA YALLANTI <228619289+Shiva13016@users.noreply.github.com>
2026-06-12 01:44:48 -0400

    feat: add notebooks/gold/Gold_Batch_Processing_Engine.py to DAB bundle
    Initial creation of the Gold Batch Processing Engine notebook for building gold layer aggregated tables from Silver data.

---
+++ databricks/notebooks/gold/Gold_Batch_Processing_Engine.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Gold_Batch_Processing_Engine
+# MAGIC
+# MAGIC **Layer:** Gold
+# MAGIC **Purpose:** Builds Gold layer aggregated tables from Silver data.
+# MAGIC
+# MAGIC **Source path:** `/Users/shivakumaryallanti5@gmail.com/project customer 360/Gold_Batch_Processing_Engine`
+
+# COMMAND ----------
+
+print("Gold Batch Processing Engine - building gold aggregations...")
+
+# COMMAND ----------
+
+# TODO: Export notebook from workspace and replace this file.
