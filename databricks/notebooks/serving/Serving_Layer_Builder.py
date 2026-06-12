c623adf2ebb3e9df16d03e22cdf4e5ec638729b0
SHIVA YALLANTI <228619289+Shiva13016@users.noreply.github.com>
2026-06-12 01:48:23 -0400

    feat: add notebooks/serving/Serving_Layer_Builder.py to DAB bundle
    Initial creation of Serving Layer Builder notebook for building serving layer views from Gold data.

---
+++ databricks/notebooks/serving/Serving_Layer_Builder.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Serving_Layer_Builder
+# MAGIC
+# MAGIC **Layer:** Serving
+# MAGIC **Purpose:** Builds serving layer views/feature tables from Gold data.
+# MAGIC
+# MAGIC **Source path:** `/Users/shivakumaryallanti5@gmail.com/project customer 360/Serving_Layer_Builder`
+
+# COMMAND ----------
+
+print("Serving Layer Builder - creating serving tables...")
+
+# COMMAND ----------
+
+# TODO: Export notebook from workspace and replace this file.
