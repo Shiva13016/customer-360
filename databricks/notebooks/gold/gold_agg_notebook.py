62e239fbc80e9e884e9e2fed2462d5d79de6ba73
SHIVA YALLANTI <228619289+Shiva13016@users.noreply.github.com>
2026-06-12 01:49:52 -0400

    feat: add notebooks/gold/gold_agg_notebook.py to DAB bundle
    Initial creation of the gold aggregation notebook for customer 360.

---
+++ databricks/notebooks/gold/gold_agg_notebook.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # gold_agg_notebook
+# MAGIC
+# MAGIC **Layer:** Gold
+# MAGIC **Purpose:** Builds Gold aggregation views for customer 360.
+# MAGIC
+# MAGIC **Source path:** `/Users/shivakumaryallanti5@gmail.com/project customer 360/gold_agg_notebook`
+
+# COMMAND ----------
+
+print("Gold Aggregation Notebook - building customer 360 aggregations...")
+
+# COMMAND ----------
+
+# TODO: Export notebook from workspace and replace this file.
